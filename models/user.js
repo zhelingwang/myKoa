89cc3610adbceaac4962196aaf024a074f333cc9
zheling <1006369178@qq.com>
2019-11-18 17:22:13 +0800

    use specific Collection Name

--- models/user.js
+++ models/user.js
@@ -13,6 +13,9 @@ const UserSchema = new Schema({
 //UserModel = Collection name , 命名会自动转小写并负数化 , UserModel => usermodels
 module.exports = mongoose.model('UserModel', UserSchema);
 
+//禁用自动转化Collection name , 加上第三个参数即可指定Collection name
+//module.exports = mongoose.model('UserModel', UserSchema,'UserModel');
+
 
 //other ways
 /**
